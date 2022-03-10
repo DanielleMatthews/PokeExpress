3dfc5728af92a5788ceb7c1f7ae85f7403356f17
DanielleMatthews <danielleem120@gmail.com>
2022-03-10 15:17:48 -0500

    css

--- views/Index.jsx
+++ views/Index.jsx
@@ -10,11 +10,11 @@ class Index extends React.Component{
                 <meta charset="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />                    
+                <link rel="stylesheet" href="/css/app.css" />
                 <title> Pokémon Index </title>
             </head>
             <body>
                 <div>
-                <link rel="stylesheet" href="/css/app.css" />
                     <h1> All Pokémon </h1>
                     <ul>
                         {pokemon.map((poke, i) => {
